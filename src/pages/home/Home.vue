cf5a502084f8bac93d73e31b6d453e4929a8aeb9
陶旭东 <txd_1997@163.com>
2019-09-01 17:51:49 +0800

    change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 // eslint-disable-next-line no-unused-vars
 import HomeHeader from './components/Header'
+// eslint-disable-next-line no-unused-vars
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
